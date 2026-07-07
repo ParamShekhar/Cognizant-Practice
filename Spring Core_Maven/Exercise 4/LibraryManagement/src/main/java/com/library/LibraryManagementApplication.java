9b405ed0389ce278b42677fa8a2d1264c777026e
ParamShekhar <param091105@gmail.com>
2026-07-07 22:27:36 +0530

    Delete Spring Core_Maven directory

--- Spring Core_Maven/Exercise 4/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
+++
@@ -1,8 +0,0 @@
-package com.library;
-
-public class LibraryManagementApplication {
-
-    public static void main(String[] args) {
-        System.out.println("LibraryManagement Maven project is set up with Spring Context, AOP, and WebMVC dependencies.");
-    }
-}
